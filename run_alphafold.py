8828b4df327517bda7a5ccbab5445ca2c633fc07
Seb-Leb <seb@leb.cat>
2021-09-13 14:55:22 -0400

    split MSA and alphafold

---
+++ run_alphafold.py
@@ -0,0 +1,377 @@
+
+#@title run alphafold
+num_relax = "None"
+rank_by = "pLDDT" #@param ["pLDDT","pTMscore"]
+use_turbo = True #@param {type:"boolean"}
+max_msa = "512:1024" #@param ["512:1024", "256:512", "128:256", "64:128", "32:64"]
+max_msa_clusters, max_extra_msa = [int(x) for x in max_msa.split(":")]
+
+
+
+#@markdown - `rank_by` specify metric to use for ranking models (For protein-protein complexes, we recommend pTMscore)
+#@markdown - `use_turbo` introduces a few modifications (compile once, swap params, adjust max_msa) to speedup and reduce memory requirements. Disable for default behavior.
+#@markdown - `max_msa` defines: `max_msa_clusters:max_extra_msa` number of sequences to use. When adjusting after GPU crash, be sure to `Runtime` → `Restart runtime`. (Lowering will reduce GPU requirements, but may result in poor model quality. This option ignored if `use_turbo` is disabled)
+show_images = True #@param {type:"boolean"}
+#@markdown - `show_images` To make things more exciting we show images of the predicted structures as they are being generated. (WARNING: the order of images displayed does not reflect any ranking).
+#@markdown ---
+#@markdown #### Sampling options
+#@markdown There are two stochastic parts of the pipeline. Within the feature generation (choice of cluster centers) and within the model (dropout).
+#@markdown To get structure diversity, you can iterate through a fixed number of random_seeds (using `num_samples`) and/or enable dropout (using `is_training`).
+
+num_models = 5 #@param [1,2,3,4,5] {type:"raw"}
+use_ptm = True #@param {type:"boolean"}
+num_ensemble = 1 #@param [1,8] {type:"raw"}
+max_recycles = 3 #@param [1,3,6,12,24,48] {type:"raw"}
+tol = 0 #@param [0,0.1,0.5,1] {type:"raw"}
+is_training = False #@param {type:"boolean"}
+num_samples = 1 #@param [1,2,4,8,16,32] {type:"raw"}
+#@markdown - `num_models` specify how many model params to try. (5 recommended)
+#@markdown - `use_ptm` uses Deepmind's `ptm` finetuned model parameters to get PAE per structure. Disable to use the original model params. (Disabling may give alternative structures.)
+#@markdown - `num_ensemble` the trunk of the network is run multiple times with different random choices for the MSA cluster centers. (`1`=`default`, `8`=`casp14 setting`)
+#@markdown - `max_recycles` controls the maximum number of times the structure is fed back into the neural network for refinement. (3 recommended)
+#@markdown  - `tol` tolerance for deciding when to stop (CA-RMS between recycles)
+#@markdown - `is_training` enables the stochastic part of the model (dropout), when coupled with `num_samples` can be used to "sample" a diverse set of structures.
+#@markdown - `num_samples` number of random_seeds to try.
+subsample_msa = True #@param {type:"boolean"}
+#@markdown - `subsample_msa` subsample large MSA to `3E7/length` sequences to avoid crashing the preprocessing protocol.
+
+save_pae_json = True
+save_tmp_pdb = True
+
+
+if use_ptm == False and rank_by == "pTMscore":
+  print("WARNING: models will be ranked by pLDDT, 'use_ptm' is needed to compute pTMscore")
+  rank_by = "pLDDT"
+
+#############################
+# delete old files
+#############################
+for f in os.listdir(output_dir):
+  if "rank_" in f:
+    os.remove(os.path.join(output_dir, f))
+
+#############################
+# homooligomerize
+#############################
+lengths = [len(seq) for seq in seqs]
+msas_mod, deletion_matrices_mod = cf.homooligomerize_heterooligomer(msas, deletion_matrices,
+                                                                    lengths, homooligomers)
+#############################
+# define input features
+#############################
+def _placeholder_template_feats(num_templates_, num_res_):
+  return {
+      'template_aatype': np.zeros([num_templates_, num_res_, 22], np.float32),
+      'template_all_atom_masks': np.zeros([num_templates_, num_res_, 37, 3], np.float32),
+      'template_all_atom_positions': np.zeros([num_templates_, num_res_, 37], np.float32),
+      'template_domain_names': np.zeros([num_templates_], np.float32),
+      'template_sum_probs': np.zeros([num_templates_], np.float32),
+  }
+
+num_res = len(full_sequence)
+feature_dict = {}
+feature_dict.update(pipeline.make_sequence_features(full_sequence, 'test', num_res))
+feature_dict.update(pipeline.make_msa_features(msas_mod, deletion_matrices=deletion_matrices_mod))
+if not use_turbo:
+  feature_dict.update(_placeholder_template_feats(0, num_res))
+
+def do_subsample_msa(F, N=10000, random_seed=0):
+  '''subsample msa to avoid running out of memory'''
+  M = len(F["msa"])
+  if N is not None and M > N:
+    print(f"whhhaaa... too many sequences ({M}) subsampling to {N}")
+    np.random.seed(random_seed)
+    idx = np.append(0,np.random.permutation(np.arange(1,M)))[:N]
+    F_ = {}
+    F_["msa"] = F["msa"][idx]
+    F_["deletion_matrix_int"] = F["deletion_matrix_int"][idx]
+    F_["num_alignments"] = np.full_like(F["num_alignments"],N)
+    for k in ['aatype', 'between_segment_residues',
+              'domain_name', 'residue_index',
+              'seq_length', 'sequence']:
+              F_[k] = F[k]
+    return F_
+  else:
+    return F
+
+################################
+# set chain breaks
+################################
+Ls = []
+for seq,h in zip(ori_sequence.split(":"),homooligomers):
+  Ls += [len(s) for s in seq.split("/")] * h
+
+Ls_plot = sum([[len(seq)]*h for seq,h in zip(seqs,homooligomers)],[])
+
+feature_dict['residue_index'] = cf.chain_break(feature_dict['residue_index'], Ls)
+
+###########################
+# run alphafold
+###########################
+def parse_results(prediction_result, processed_feature_dict):
+  b_factors = prediction_result['plddt'][:,None] * prediction_result['structure_module']['final_atom_mask']
+  dist_bins = jax.numpy.append(0,prediction_result["distogram"]["bin_edges"])
+  dist_mtx = dist_bins[prediction_result["distogram"]["logits"].argmax(-1)]
+  contact_mtx = jax.nn.softmax(prediction_result["distogram"]["logits"])[:,:,dist_bins < 8].sum(-1)
+
+  out = {"unrelaxed_protein": protein.from_prediction(processed_feature_dict, prediction_result, b_factors=b_factors),
+         "plddt": prediction_result['plddt'],
+         "pLDDT": prediction_result['plddt'].mean(),
+         "dists": dist_mtx,
+         "adj": contact_mtx}
+  if "ptm" in prediction_result:
+    out.update({"pae": prediction_result['predicted_aligned_error'],
+                "pTMscore": prediction_result['ptm']})
+  return out
+
+model_names = ['model_1', 'model_2', 'model_3', 'model_4', 'model_5'][:num_models]
+total = len(model_names) * num_samples
+with tqdm.notebook.tqdm(total=total, bar_format=TQDM_BAR_FORMAT) as pbar:
+  #######################################################################
+  # precompile model and recompile only if length changes
+  #######################################################################
+  if use_turbo:
+    name = "model_5_ptm" if use_ptm else "model_5"
+    N = len(feature_dict["msa"])
+    L = len(feature_dict["residue_index"])
+    compiled = (N, L, use_ptm, max_recycles, tol, num_ensemble, max_msa, is_training)
+    if "COMPILED" in dir():
+      if COMPILED != compiled: recompile = True
+    else: recompile = True
+    if recompile:
+      cf.clear_mem(device)
+      cfg = config.model_config(name)
+
+      # set size of msa (to reduce memory requirements)
+      msa_clusters = min(N, max_msa_clusters)
+      cfg.data.eval.max_msa_clusters = msa_clusters
+      cfg.data.common.max_extra_msa = max(min(N-msa_clusters,max_extra_msa),1)
+
+      cfg.data.common.num_recycle = max_recycles
+      cfg.model.num_recycle = max_recycles
+      cfg.model.recycle_tol = tol
+      cfg.data.eval.num_ensemble = num_ensemble
+
+      params = data.get_model_haiku_params(name,'./alphafold/data')
+      model_runner = model.RunModel(cfg, params, is_training=is_training)
+      COMPILED = compiled
+      recompile = False
+
+  else:
+    cf.clear_mem(device)
+    recompile = True
+
+  # cleanup
+  if "outs" in dir(): del outs
+  outs = {}
+  cf.clear_mem("cpu")
+
+  #######################################################################
+  for num, model_name in enumerate(model_names): # for each model
+    name = model_name+"_ptm" if use_ptm else model_name
+
+    # setup model and/or params
+    params = data.get_model_haiku_params(name, './alphafold/data')
+    if use_turbo:
+      for k in model_runner.params.keys():
+        model_runner.params[k] = params[k]
+    else:
+      cfg = config.model_config(name)
+      cfg.data.common.num_recycle = cfg.model.num_recycle = max_recycles
+      cfg.model.recycle_tol = tol
+      cfg.data.eval.num_ensemble = num_ensemble
+      model_runner = model.RunModel(cfg, params, is_training=is_training)
+
+    for seed in range(num_samples): # for each seed
+      # predict
+      key = f"{name}_seed_{seed}"
+      pbar.set_description(f'Running {key}')
+      if subsample_msa:
+        subsampled_N = int(3E7/L)
+        sampled_feats_dict = do_subsample_msa(feature_dict, N=subsampled_N, random_seed=seed)
+        processed_feature_dict = model_runner.process_features(sampled_feats_dict, random_seed=seed)
+      else:
+        processed_feature_dict = model_runner.process_features(feature_dict, random_seed=seed)
+
+      prediction_result, (r, t) = cf.to(model_runner.predict(processed_feature_dict, random_seed=seed),"cpu")
+      outs[key] = parse_results(prediction_result, processed_feature_dict)
+
+      # report
+      pbar.update(n=1)
+      line = f"{key} recycles:{r} tol:{t:.2f} pLDDT:{outs[key]['pLDDT']:.2f}"
+      if use_ptm: line += f" pTMscore:{outs[key]['pTMscore']:.2f}"
+      print(line)
+      if show_images:
+        fig = cf.plot_protein(outs[key]["unrelaxed_protein"], Ls=Ls_plot, dpi=100)
+        plt.ion()
+      if save_tmp_pdb:
+        tmp_pdb_path = os.path.join(output_dir,f'unranked_{key}_unrelaxed.pdb')
+        pdb_lines = protein.to_pdb(outs[key]["unrelaxed_protein"])
+        with open(tmp_pdb_path, 'w') as f: f.write(pdb_lines)
+
+
+      # cleanup
+      del processed_feature_dict, prediction_result
+      if subsample_msa: del sampled_feats_dict
+
+    if use_turbo:
+      del params
+    else:
+      del params, model_runner, cfg
+      cf.clear_mem(device)
+
+  # delete old files
+  for f in os.listdir(output_dir):
+    if "rank" in f:
+      os.remove(os.path.join(output_dir, f))
+
+  # Find the best model according to the mean pLDDT.
+  model_rank = list(outs.keys())
+  model_rank = [model_rank[i] for i in np.argsort([outs[x][rank_by] for x in model_rank])[::-1]]
+
+  # Write out the prediction
+  for n,key in enumerate(model_rank):
+    prefix = f"rank_{n+1}_{key}"
+    pred_output_path = os.path.join(output_dir,f'{prefix}_unrelaxed.pdb')
+    fig = cf.plot_protein(outs[key]["unrelaxed_protein"], Ls=Ls_plot, dpi=200)
+    plt.savefig(os.path.join(output_dir,f'{prefix}.png'), bbox_inches = 'tight')
+    plt.close(fig)
+
+    pdb_lines = protein.to_pdb(outs[key]["unrelaxed_protein"])
+    with open(pred_output_path, 'w') as f:
+      f.write(pdb_lines)
+
+############################################################
+print(f"model rank based on {rank_by}")
+for n,key in enumerate(model_rank):
+  print(f"rank_{n+1}_{key} {rank_by}:{outs[key][rank_by]:.2f}")
+  if use_ptm and save_pae_json:
+    pae = outs[key]["pae"]
+    max_pae = pae.max()
+    # Save pLDDT and predicted aligned error (if it exists)
+    pae_output_path = os.path.join(output_dir,f'rank_{n+1}_{key}_pae.json')
+    # Save predicted aligned error in the same format as the AF EMBL DB
+    rounded_errors = np.round(np.asarray(pae), decimals=1)
+    indices = np.indices((len(rounded_errors), len(rounded_errors))) + 1
+    indices_1 = indices[0].flatten().tolist()
+    indices_2 = indices[1].flatten().tolist()
+    pae_data = json.dumps([{
+        'residue1': indices_1,
+        'residue2': indices_2,
+        'distance': rounded_errors.flatten().tolist(),
+        'max_predicted_aligned_error': max_pae.item()
+    }],
+                          indent=None,
+                          separators=(',', ':'))
+    with open(pae_output_path, 'w') as f:
+      f.write(pae_data)
+#%%
+#@title Refine structures with Amber-Relax (Optional)
+#@markdown If side-chain bond geometry is important to you, enable Amber-Relax by specifying how many top ranked structures you want relaxed. By default, we disable Amber-Relax since it barely moves the main-chain (backbone) structure and can overall double the runtime.
+num_relax = "Top5" #@param ["None", "Top1", "Top5", "All"] {type:"string"}
+if num_relax == "None":
+  num_relax = 0
+elif num_relax == "Top1":
+  num_relax = 1
+elif num_relax == "Top5":
+  num_relax = 5
+else:
+  num_relax = len(model_names) * num_samples
+
+if num_relax > 0:
+  if "relax" not in dir():
+    # add conda environment to path
+    sys.path.append('./colabfold-conda/lib/python3.7/site-packages')
+
+    # import libraries
+    from alphafold.relax import relax
+    from alphafold.relax import utils
+
+  with tqdm.notebook.tqdm(total=num_relax, bar_format=TQDM_BAR_FORMAT) as pbar:
+    pbar.set_description(f'AMBER relaxation')
+    for n,key in enumerate(model_rank):
+      if n < num_relax:
+        prefix = f"rank_{n+1}_{key}"
+        pred_output_path = os.path.join(output_dir,f'{prefix}_relaxed.pdb')
+        if not os.path.isfile(pred_output_path):
+          amber_relaxer = relax.AmberRelaxation(
+              max_iterations=0,
+              tolerance=2.39,
+              stiffness=10.0,
+              exclude_residues=[],
+              max_outer_iterations=20)
+          relaxed_pdb_lines, _, _ = amber_relaxer.process(prot=outs[key]["unrelaxed_protein"])
+          with open(pred_output_path, 'w') as f:
+            f.write(relaxed_pdb_lines)
+        pbar.update(n=1)
+#%%
+#@title Display 3D structure {run: "auto"}
+rank_num = 1 #@param ["1", "2", "3", "4", "5"] {type:"raw"}
+color = "lDDT" #@param ["chain", "lDDT", "rainbow"]
+show_sidechains = False #@param {type:"boolean"}
+show_mainchains = False #@param {type:"boolean"}
+
+key = model_rank[rank_num-1]
+prefix = f"rank_{rank_num}_{key}"
+pred_output_path = os.path.join(output_dir,f'{prefix}_relaxed.pdb')
+if not os.path.isfile(pred_output_path):
+  pred_output_path = os.path.join(output_dir,f'{prefix}_unrelaxed.pdb')
+
+cf.show_pdb(pred_output_path, show_sidechains, show_mainchains, color, Ls=Ls_plot).show()
+if color == "lDDT": cf.plot_plddt_legend().show()
+if use_ptm:
+  cf.plot_confidence(outs[key]["plddt"], outs[key]["pae"], Ls=Ls_plot).show()
+else:
+  cf.plot_confidence(outs[key]["plddt"], Ls=Ls_plot).show()
+#%%
+#@title Extra plots
+dpi =  300#@param {type:"integer"}
+save_to_txt = True #@param {type:"boolean"}
+#@markdown - save data used to generate plots below to text file
+
+
+if use_ptm:
+  print("predicted alignment error")
+  cf.plot_paes([outs[k]["pae"] for k in model_rank], Ls=Ls_plot, dpi=dpi)
+  plt.savefig(os.path.join(output_dir,f'predicted_alignment_error.png'), bbox_inches = 'tight', dpi=np.maximum(200,dpi))
+  # plt.show()
+
+print("predicted contacts")
+cf.plot_adjs([outs[k]["adj"] for k in model_rank], Ls=Ls_plot, dpi=dpi)
+plt.savefig(os.path.join(output_dir,f'predicted_contacts.png'), bbox_inches = 'tight', dpi=np.maximum(200,dpi))
+# plt.show()
+
+print("predicted distogram")
+cf.plot_dists([outs[k]["dists"] for k in model_rank], Ls=Ls_plot, dpi=dpi)
+plt.savefig(os.path.join(output_dir,f'predicted_distogram.png'), bbox_inches = 'tight', dpi=np.maximum(200,dpi))
+# plt.show()
+
+print("predicted LDDT")
+cf.plot_plddts([outs[k]["plddt"] for k in model_rank], Ls=Ls_plot, dpi=dpi)
+plt.savefig(os.path.join(output_dir,f'predicted_LDDT.png'), bbox_inches = 'tight', dpi=np.maximum(200,dpi))
+# plt.show()
+
+def do_save_to_txt(filename, adj, dists, pae=None, Ls=None):
+  adj = np.asarray(adj)
+  dists = np.asarray(dists)
+  if pae is not None: pae = np.asarray(pae)
+  L = len(adj)
+  with open(filename,"w") as out:
+    header = "i\tj\taa_i\taa_j\tp(cbcb<8)\tmaxdistbin"
+    header += "\tpae_ij\tpae_ji\n" if pae is not None else "\n"
+    out.write(header)
+    for i in range(L):
+      for j in range(i+1,L):
+        line = f"{i+1}\t{j+1}\t{full_sequence[i]}\t{full_sequence[j]}\t{adj[i][j]:.3f}"
+        line += f"\t>{dists[i][j]:.2f}" if dists[i][j] == 21.6875 else f"\t{dists[i][j]:.2f}"
+        if pae is not None: line += f"\t{pae[i][j]:.3f}\t{pae[j][i]:.3f}"
+        out.write(f"{line}\n")
+
+if save_to_txt:
+  for n,k in enumerate(model_rank):
+    txt_filename = os.path.join(output_dir,f'rank_{n+1}_{k}.raw.txt')
+    if use_ptm:
+      do_save_to_txt(txt_filename,adj=outs[k]["adj"],dists=outs[k]["dists"],pae=outs[k]["pae"])
+    else:
+      do_save_to_txt(txt_filename,adj=outs[k]["adj"],dists=outs[k]["dists"])
+#%%
